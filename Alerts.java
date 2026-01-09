22f5986b6fb1740a83714392d27a4f99886ecdaa
Dhanoj01 <149933983+Dhanoj01@users.noreply.github.com>
2026-01-09 10:47:27 +0530

    Update Alerts.java

--- Alerts.java
+++ Alerts.java
@@ -1,4 +1,28 @@
 
 public class Alerts {
 
+  public static void main(String args[])
+  {
+     WebDriver driver = new ChromeDriver();
+    driver.get("");
+    driver.manage().window().maximise();
+
+    //simple alert
+    driver.findElement(By.xpath("fdinfdkj")).click();
+    driver.switchTo().alert().accept();
+
+
+    //confirm box alert
+    driver.findElement(By.xpath("button[[]""ref)).click();
+    driver.switchTo().alert().dismiss();
+    
+    //prompt alert
+    driver.findElement(By.ClassName(alert-box)).click();
+    driver.switchTo().alert().sendKeys("HII");
+    driver.switchTo().alert().accept();
+
+    driver.quit();
+  
+  }
 }
+
